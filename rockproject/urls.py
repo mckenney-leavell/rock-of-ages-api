c9084a0fc457a1ff9fa6416a7580d8635c14dd76
McKenney Leavell <maleavell@gmail.com>
2025-11-07 10:59:26 -0600

    Database seeded

--- rockproject/urls.py
+++ rockproject/urls.py
@@ -1,12 +1,12 @@
 from django.contrib import admin
 from django.urls import include, path
 from rest_framework import routers
-from rockapi.views import register_user, login_user, TypeView
+from rockapi.views import register_user, login_user, TypeView, RockView
 
 
 router = routers.DefaultRouter(trailing_slash=False)
 router.register(r'types', TypeView, 'type')
-router.register(r'rocks', TypeView, 'rock')
+router.register(r'rocks', RockView, 'rock')
 
 urlpatterns = [
     path('', include(router.urls)),
